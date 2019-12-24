d2732cd858ce3382c51b49d6ef57fd6ec24640cf
Rukmini Bapat <rukminibapat@Rukminis-MacBook-Air.local>
2019-12-24 15:44:07 -0800

    buggy version

--- setTheory.cpp
+++ setTheory.cpp
@@ -37,6 +37,10 @@ Myset* get_union(int* arr, int len1, int* arr2, int len2){
     state = true;
   }
   cout<<count<<endl; 
+  for(int k = 0; k<count; k++){
+    cout<<"   "<<localarr[k];
+  }
+  cout<<"_____"<<endl;
   int* heaparr = new int[count];
   for(int k = 0; k<count; k++){
     heaparr[k] = localarr[k];
@@ -49,14 +53,50 @@ Myset* get_union(int* arr, int len1, int* arr2, int len2){
  
 }
 
+Myset* get_inter(int* arr, int len1, int* arr2, int len2){
+  Myset* mydata = new Myset;
+  int localarr[len1+len2];
+  int counter = 0;
+  bool exists = false;
+  int element = 0;
+  for(int i = 0; i<len1; i++){
+   for(int j =0; j<len2; j++){
+     if(arr[i] == arr2[j]){
+       exists = true;
+       element = arr[i];
+     }      
+   }
+   if(exists == true){
+     localarr[counter] = element;
+     counter++;
+     exists = false;
+   }
+  }
+  for(int f = 0; f<counter; f++){ 
+    cout<<"__"<<localarr[f];
+  }
+  cout<<endl;
+  mydata->myList = new int[counter];
+  for(int k = 0; k<counter; k++){
+     mydata->myList[k] = localarr[k];
+  }
+  mydata->count = counter;
+ 
+  return mydata; 
+}
+
 int main(){
-  int arr[] = {1,2,3,4, 5};
-  int arr2[] = {2,3,5,7};
+  int arr[] = { 6,7,10, 19};
+  int arr2[] = {6,19};
   //expected union: 1, 2, 3, 4, 5, 7
   //expected intersection: 2, 3, 5
-  Myset* answer = get_union(arr, 5, arr2, 4);
+  Myset* answer = get_union(arr, 4, arr2, 2);
   for(int i = 0; i<answer->count; i++){ 
      cout<<answer->myList[i]<<" ";
   }
-  //int* unionarr = gen_union(arr, 5, arr2, 4);
+  cout<<endl;
+  Myset* intersection = get_inter(arr,4, arr2, 2);
+  for(int j = 0; j<intersection->count; j++){
+    cout<<intersection->myList[j]<<" ";
+  } 
 } 
