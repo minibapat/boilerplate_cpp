e59757070adc87a52956367525a3e86cf4d1117a
Rukmini Bapat <rukminibapat@Rukminis-MacBook-Air.local>
2020-01-04 16:31:54 -0800

    Solved Daily Coding Problem 2. Added flexibility to command line arguments

---
+++ dcp2.cpp
@@ -0,0 +1,40 @@
+//Given an array of integers, return a new array such that each element 
+//at index i of the new array is the product of all the numbers in the 
+//original array except the one at i
+//Daily Coding Problem #2 from dailycodingproblem.com
+
+#include <iostream>
+using namespace std;
+
+int* findprod(int* arr, int len);
+
+int main(int argc, char* argv[]){
+	if(argc < 3){
+		cerr<<"Usage: "<<argv[0]<<" Enter a list of numbers separated by spaces"<<endl;
+		exit(1);
+	}
+	int arr[argc-1];
+	for(int i = 0; i<argc-1; i++){
+		arr[i] = atoi(argv[i+1]);
+	}
+	int len = sizeof(arr)/sizeof(arr[0]);
+	int* newarr = findprod(arr, len);
+	for(int k = 0; k<len; k++){
+		cout<<newarr[k]<<" ";
+	}
+ 	cout<<endl;
+}
+
+int* findprod(int* arr, int len){
+	int* ptr = new int[len];
+	for(int i = 0; i<len; i++){
+		int product = 1;
+  	for(int j = 0; j<len; j++){
+			if(j != i){
+			 product = product * arr[j];
+			}
+		}	
+		ptr[i] = product;
+  }
+	return ptr;
+}
