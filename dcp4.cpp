5a62fbc459b43238451730ef8f3cd3cfa5f75800
Rukmini Bapat <rukminibapat@169-231-97-47.wireless.ucsb.edu>
2020-01-06 09:32:50 -0800

    Solved Daily Coding Problem #4

--- dcp4.cpp
+++ dcp4.cpp
@@ -1,4 +1,5 @@
-/*Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+/*Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest
+positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
 For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3. 
 This is daily coding problem #4 asked by dailycodingproblem.com */
 
