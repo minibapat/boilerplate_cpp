1577cee16ab4e1dc10f7f8d0610997e8d1f2fd6d
minibapat <mini.bapat@gmail.com>
2020-04-04 17:54:52 -0700

    Update closed_hashing.cpp

--- closed_hashing.cpp
+++ closed_hashing.cpp
@@ -25,7 +25,9 @@ class closedMap {
 };
 
 int main() {
-	static string a[] = {"avenal", "costa mesa", "paso robles", "mesa verde"};
+	static string a[] = {"avenal", "costa mesa", "paso robles", "mesa verde", "anaheim"};
+	//anaheim collides with avenal because both start at index 0 (a) so anaheim will replace avenal. We fix this 
+	//drawback by using open hashing.
 	closedMap cm;
 	for(int jj = 0; jj<sizeof(a)/sizeof(a[0]); jj++) {
 		cm.insert(a[jj]);
